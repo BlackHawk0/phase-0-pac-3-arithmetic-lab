5ec2a98165cf8354778c1b99329d671446b4305d
Wanambuko Ngoya Daniel <danielngoys@outlook.com>
2022-08-04 11:19:20 +0300

    working with parseInt and parseFloat

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a,b){
+    return a+b;
+}
+function subtract(q,b){
+    return q-b;
+}
+function multiply(j,k){
+    return j*k;
+}
+function divide(p,q){
+    return p/q;
+}
+function increment(n){
+    return n+1;
+}
+function decrement(n){
+    return n-1;
+}
+
+function makeInt(n){
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
